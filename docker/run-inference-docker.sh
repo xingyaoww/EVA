64bd02012661500fc7c2d31eaf72f67419c07aea
Xingyao <xingyaow@umich.edu>
2022-06-26 02:34:52 +0800

    feat: run docker in detach model

--- docker/run-inference-docker.sh
+++ docker/run-inference-docker.sh
@@ -1,7 +1,9 @@
 docker run \
+    -d \
     -p 6666:6666 \
     --gpus 0 \
     -v ${PWD}:/home/coai/EVA \
+    --name EVA2-inference-server \
     eva:1.5.cu113 /bin/bash /home/coai/EVA/src/scripts/eva_inference_server_no_beam.sh
 
 # Example Request:
